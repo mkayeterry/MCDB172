0793e4483a42f9ca8d65c91042b9279e1628515e
Melissa Terry <mkterry@ucsb.edu>
2023-02-26 10:07:18 -0800

    mid hw push

--- py/hw/HW8_test.py
+++ py/hw/HW8_test.py
@@ -1,207 +1,119 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-
-def ImperfectImmunity(state, t):
-    I = state[0]
-    S = state[1]
-
-    # define system with imperfect immunity
-    dIdt = alpha * I * S - I/tau_inf
-    R = N - I - S 
-    dSdt = -alpha * I * S + R/tau_imm 
-
-    return(dIdt, dSdt)
-
-def Rt(N, It, St):
-    Nvec = N * np.ones(St.shape)
-    return (Nvec - St - It)
-
-# nullcline functions
-def I_nullcline(S):
-    return ( 1 / (tau_inf * alpha * S) )
-
-def S_nullcline(I):
-    return ( (N - I) / (tau_imm * alpha) )
 
 # define parameters
 alpha = 0.4
 tau_inf = 0.1
 tau_imm = 3
-
-state0 = [1,99]
-
-N = np.sum(state0)
-t = np.linspace(0, 10, 1000)
-
-state = odeint(ImperfectImmunity, state0, t)
-
-# plotting a phase diagram
-Ilim = [0, 100]
-Slim = [0, 100]
-
-npoints = 30 # pick the resolution of our phase portrait
-
-# set up meshgrid function for evaluating the dynamics at many states
-h1 = np.linspace(Ilim[0], Ilim[1], npoints)
-h2 = np.linspace(Ilim[0], Ilim[1], npoints)
-
-# generate meshgrid
-H1, H2 = np.meshgrid(h1, h2)
-
-u, v = np.zeros(H1.shape), np.zeros(H2.shape)
-
-NJ, NK = H1.shape
-
-# two nested for loops to evaluate the dynamics on the meshgrid
-for j in range(NJ):
-    for k in range(NK):
-
-        Istate = H1[j, k]
-        Sstate = H2[j, k]
-
-        statejk = [Istate, Sstate]
-
-        # call differential equation to calc system velocity
-        ISdot = ImperfectImmunity(statejk, [])
-
-        u[j,k] = ISdot[0]
-        v[j,k] = ISdot[1]
-
-
-# normalize vector lengths by the hypoteneuse
-M = (np.hypot(u,v))
-M[M==0] = 1.
-u /= M
-v /= M
-
-plt.quiver(H1, H2, u, v, color = 'red')
-plt.xlabel('Infected Indiviuals', fontsize =15)
-plt.ylabel('Susceptible Indiviuals', fontsize =15)
-plt.plot(state[:,0], state[:,1], linewidth = 5)
-plt.plot(np.linspace(0, 100, 100), I_nullcline(state[0]), linewidth = 5, label = 'I Nullcline')
-plt.plot(np.linspace(0, 100, 100), S_nullcline(state[1]), linewidth = 5, label = 'S Nullcline')
-# plt.legend(loc = 'upper right')
-plt.ylim((0,100))
-plt.xlim((0,100))
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-
-def ImperfectImmunity(state, t):
-    I = state[0]
-    S = state[1]
-
-    # define system with imperfect immunity
-    dIdt = alpha * I * S - I/tau_inf
-    R = N - I - S 
-    dSdt = -alpha * I * S + R/tau_imm 
-
-    return(dIdt, dSdt)
-
-def Rt(N, It, St):
-    Nvec = N * np.ones(St.shape)
-    return (Nvec - St - It)
+N = 100
 
 # nullcline function
 def get_nullclines(S, I):
-    I_nullcline = 1 / (tau_inf * alpha * S)
-    S_nullcline = (N - I) / (tau_imm * alpha)
-    return (S_nullcline, I_nullcline)
+
+    for i in np.where(tau_inf * alpha * S != 0):
+        I_nullcline = 1 / (tau_inf * alpha * S)
+        S_nullcline = (N - I) / (tau_imm * alpha)
+        return (S_nullcline, I_nullcline)
+
+# define axis ranges
+I_range = np.arange(0, 100)
+S_range = np.arange(0, 100)
+
+# meshgrid for phase diagram
+I_grid, S_grid = np.meshgrid(I_range, S_range)
+
+# calculate derivatives
+dI_dt = alpha * I_grid * S_grid - (I_grid / tau_inf)
+R = (N - I_grid - S_grid)
+dS_dt = -alpha * I_grid * S_grid + R / tau_imm
+
+# plot phase space diagram
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.quiver(I_grid, S_grid, dI_dt, dS_dt, color='#440154')
+ax.plot(I_range, get_nullclines(S_range, I_range)[1], linewidth = 5, label='I-nullcline', color='#5ec962')
+ax.plot(I_range, get_nullclines(S_range, I_range)[0], linewidth = 5, label='S-nullcline', color='#3b528b')
+ax.set_xlabel('dIdt', fontsize =15)
+ax.set_ylabel('dSdt', fontsize =15)
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 100)
+ax.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 # define parameters
 alpha = 0.4
 tau_inf = 0.1
 tau_imm = 3
-state0 = [1,99]
-N = np.sum(state0)
-t = np.linspace(0, 10, 1000)
+N = 100
 
-state = odeint(ImperfectImmunity, state0, t)
+# nullcline functions
+def get_nullclines(S, I):
+    I_nullcline = 1 / (alpha * tau_inf)
+    S_nullcline = tau_inf * (N - 1 / (alpha * tau_inf)) / (tau_imm + tau_inf)
+    return (S_nullcline, I_nullcline)
 
-# plotting a phase diagram
-I_lim = [0, 100]
-S_lim = [0, 100]
+# define axis ranges
+I_range = np.linspace(0, 100, 100)
+S_range = np.linspace(0, 100, 100)
 
-npoints = 30 # pick the resolution of our phase portrait
-
-# set up meshgrid function for evaluating the dynamics at many states
-I_range = np.linspace(I_lim[0], I_lim[1], npoints)
-S_range = np.linspace(I_lim[0], I_lim[1], npoints)
-
-# generate meshgrid
+# meshgrid for phase diagram
 I_grid, S_grid = np.meshgrid(I_range, S_range)
 
-I_vel, S_vel = np.zeros(I_grid.shape), np.zeros(S_grid.shape)
-
-I_points, S_points = I_grid.shape
-
-
-# two nested for loops to evaluate the dynamics on the meshgrid
-for i in range(I_points):
-    for j in range(S_points):
-
-        I_state = I_grid[i, j]
-        S_state = S_grid[i, j]
-
-        current_state = [I_state, S_state]
-
-        # call differential equation to calc system velocity
-        ISdot = ImperfectImmunity(current_state, [])
-
-        I_vel[i,j] = ISdot[0]
-        S_vel[i,j] = ISdot[1]
+# calculate derivatives
+dI_dt = alpha * I_grid * S_grid - (I_grid / tau_inf)
+R = (N - I_grid - S_grid)
+dS_dt = -alpha * I_grid * S_grid + R / tau_imm
 
 # normalize vector lengths by the hypoteneuse
-hypo_len = (np.hypot(I_vel,S_vel))
+hypo_len = (np.hypot(dI_dt,dS_dt))
 hypo_len[hypo_len==0] = 1.0
-I_vel /= hypo_len
-S_vel /= hypo_len
+dI_dt /= hypo_len
+dS_dt /= hypo_len
 
-# initialize arrays to store the nullclines
-S_nullclines = np.zeros_like(I_grid)
-I_nullclines = np.zeros_like(S_grid)
-
-# two nested for loops to evaluate the dynamics on the meshgrid
-for i in range(I_points):
-    for j in range(S_points):
-
-        I_state = I_grid[i, j]
-        S_state = S_grid[i, j]
-
-        # evaluate the nullclines at the current state
-        S_nullcline, I_nullcline = get_nullclines(S_state, I_state)
-
-        # store the nullclines in arrays
-        S_nullclines[i, j] = S_nullcline
-        I_nullclines[i, j] = I_nullcline
-
-plt.quiver(I_grid, S_grid, I_vel, S_vel, color = 'red')
-plt.xlabel('Infected Indiviuals', fontsize =15)
-plt.ylabel('Susceptible Indiviuals', fontsize =15)
-plt.plot(state[:,0], state[:,1], linewidth = 5)
-plt.plot(np.linspace(0, 100, 100), I_nullclines, linewidth = 5, label = 'I Nullcline')
-# plt.plot(np.linspace(0, 100, 100), S_nullclines, linewidth = 5, label = 'S Nullcline')
-# plt.legend(loc = 'upper right')
-# plt.ylim((0,100))
-# plt.xlim((0,100))
+# plot phase space diagram
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.quiver(I_grid, S_grid, dI_dt, dS_dt, scale = 100, color='#440154')
+ax.plot(I_range, get_nullclines(S_range, I_range)[1]*np.ones(100), linewidth = 3, label='I-nullcline', color='#5ec962')
+ax.plot(get_nullclines(S_range, I_range)[0]*np.ones(100), S_range, linewidth = 3, label='S-nullcline', color='#3b528b')
+ax.set_xlabel('dIdt', fontsize =15)
+ax.set_ylabel('dSdt', fontsize =15)
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 100)
+ax.legend()
 plt.show()
+
+
+
+
+
+
+import sympy as sym
+
+# Define symbols
+I, S, alpha, tau_inf, tau_imm, N = sym.symbols('I S alpha tau_inf tau_imm N')
+
+# Define the equations
+eq1 = alpha * I * S - I / tau_inf
+eq2 = -alpha * I * S + (N - I - S) / tau_imm
+
+# Solve for I_dot = 0 and S_dot = 0
+sol = sym.solve((eq1, eq2), (I, S))
+
+# Print the solutions
+print(sol)
