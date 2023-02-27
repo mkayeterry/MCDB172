864c6ae3db6f41d6df4ebeee488ffb8c61f4d0e6
Melissa Terry <mkterry@ucsb.edu>
2023-02-27 13:54:45 -0800

    mid project save

--- py/hw/HW8_test.py
+++ py/hw/HW8_test.py
@@ -3,61 +3,38 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from math import sin
 
-def ImperfectImmunity(state, t):
-    # alpha = a * (np.sin(t) + 1)
+def ImperfectImmunity_latent(state, t):
     I = state[0]
-    S = state[1]
+    X = state[1]
+    S = state[2]
 
-    dIdt = np.zeros(t.size)
-    dSdt = np.zeros(t.size)
 
-    for I_idx, i in enumerate(I):
-            for S_idx, s in enumerate(S):
-                dIdt[I_idx] = alpha * i * s - i/tau
-                R = N - i - s
-                dSdt[S_idx] = -alpha * i * s + R/tau2
+    # define system with imperfect immunity and a latent period
+    dIdt = a * I * S - I/tau_inf
+    dXdt =  a * (I + X) * S - X/tau_lat
+    R = N - I - S 
+    dSdt = -a * I * S + R/tau_imm
 
-    return(dIdt, dSdt)
-
-def Rt(N, It, St):
-    Nvec = N * np.ones(St.shape)
-    return (Nvec - St - It)
-
-# nullcline function
-def get_nullclines():
-    # alpha = a * (np.sin(t) + 1)
-    I_nullcline = 1 / (alpha * tau)
-    S_nullcline = tau * (N - 1 / (alpha * tau)) / (tau2 + tau)
-    return (S_nullcline, I_nullcline)
+    return(dIdt, dXdt, dSdt)
 
 # define parameters
-# alpha = 0.4
-tau = .1
-tau2 = 3
-state0 = [1,99]
+a = .4
+tau_inf = .1
+tau_imm = 3
+tau_lat = tau_inf - tau_imm
+state0 = [1, 0, 99]
 N = np.sum(state0)
-t = np.linspace(0, 10, 1000)
-alpha = np.zeros(t.size)
-for i, v in enumerate(t):
-    alpha[i] = .4 * (sin(v) + 1)
+t = np.linspace(0, 20, 1000)
 
+state = odeint(ImperfectImmunity_latent, state0, t)
 
-state = odeint(ImperfectImmunity, state0, t)
-
-I_nullcline = get_nullclines()[1]
-S_nullcline = get_nullclines()[0]
-
-plt.plot(t, Rt(N, state[:,0], state[:,1]), c='#77dd77', linewidth = 3, label = 'Recovered')
-plt.plot(t, state[:, 0], c='#ff6961', linewidth = 3, label = 'Infected')
-plt.plot(t, state[:, 1], c='#7b519d', linewidth = 3, label = 'Susceptible')
-plt.plot(t, I_nullcline*np.ones(1000), c='#5B2C6F', linestyle = '--', linewidth = 2, label='I nullcline')
-plt.plot(t, S_nullcline*np.ones(1000), c='#953732', linestyle = '--', linewidth = 2, label='S nullcline')
-plt.legend(fontsize = 10, loc = 'upper right')
-plt.xlabel('Time', fontsize =15)
-plt.ylabel('Individuals', fontsize =15)
+plt.plot(t, state[:, 0], c='#ff6961', linewidth=3, label='Infected')
+plt.plot(t, state[:, 1], c='#FFA500', linewidth=3, label='Exposed')
+plt.plot(t, state[:, 2], c='#7b519d', linewidth=3, label='Susceptible')
+plt.legend(fontsize=10, loc='upper right')
+plt.xlabel('Time', fontsize=15)
+plt.ylabel('Individuals', fontsize=15)
+plt.xlim(-.2, 10)
+plt.ylim(-1, 100)
 plt.show()
-
-
-
-
 
