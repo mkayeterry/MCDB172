9d6a24ffc326a8e743791b0e43c84f80cadce48d
Melissa Terry <mkterry@ucsb.edu>
2023-02-27 16:18:27 -0800

    working on plotting differentials with secondary axis

--- final/final.py
+++ final/final.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import integrate
+
 ## Variables:
 
 # E = golden eagle (predator)
@@ -53,3 +57,45 @@ dFdt = ( r_f * F * (1 - ((F + (beta_fs * S)) / K_f)) ) - ( mu_f * ((phi * F) / (
 dSdt = ( r_s * S * (1 - ((S + (beta_sf * F)) / K_s)) ) - ( mu_s * ((sigma * S) / ((phi * F) + (sigma * S) + P)) * E * S )
 dPdt = ( r_p * P * (1 - (P / K_p)) ) - ( mu_p * (P / ((phi * F) + (sigma * S) + P)) * E * P )
 dEdt = (((lambda_f * mu_f * phi * (F**2)) + (lambda_s * mu_s * sigma * (S**2)) + (lambda_p * mu_p * (P**2)) * E) / ((phi * F) + (sigma * S) + P)) - (nu * E)
+
+# function to calculate derivatives
+def derivative(X, t, phi, sigma, nu, r_f, r_s, r_p, K_f, K_s, K_p, mu_f, mu_s, mu_p, lambda_f, lambda_s, lambda_p, beta_fs, beta_sf):
+    F, S, P, E = X
+    dFdt = ( r_f * F * (1 - ((F + (beta_fs * S)) / K_f)) ) - ( mu_f * ((phi * F) / ((phi * F) + (sigma * S) + P)) * E * F )
+    dSdt = ( r_s * S * (1 - ((S + (beta_sf * F)) / K_s)) ) - ( mu_s * ((sigma * S) / ((phi * F) + (sigma * S) + P)) * E * S )
+    dPdt = ( r_p * P * (1 - (P / K_p)) ) - ( mu_p * (P / ((phi * F) + (sigma * S) + P)) * E * P )
+    dEdt = (((lambda_f * mu_f * phi * (F**2)) + (lambda_s * mu_s * sigma * (S**2)) + (lambda_p * mu_p * (P**2)) * E) / ((phi * F) + (sigma * S) + P)) - (nu * E)
+    return np.array([dFdt, dSdt, dPdt, dEdt])
+
+
+# call function to calculate ODEs
+Nt = 1000
+tmax = 100
+t = np.linspace(0,tmax, Nt)
+X0 = [F0, S0, P0, E0]
+res = integrate.odeint(derivative, X0, t, args = (phi, sigma, nu, r_f, r_s, r_p, K_f, K_s, K_p, mu_f, mu_s, mu_p, lambda_f, lambda_s, lambda_p, beta_fs, beta_sf))
+F, S, P, E = res.T
+
+
+
+# without secondary axis - doesnt work
+# plt.figure()
+# plt.grid()
+# plt.title("Foxes and Skunks")
+# plt.plot(t, F, '-', linewidth = 3, label = 'Foxes')
+# plt.plot(t, S, '-', linewidth = 3, label = 'Skunks')
+# plt.xlabel('Time t, [days]')
+# plt.ylabel('Population')
+# plt.legend()
+# plt.show()
+
+
+fig, ax = plt.subplots(constrained_layout=True)
+ax.plot(t, F, '-', linewidth = 3, label = 'Foxes')
+ax.plot(t, S, '-', linewidth = 3, label = 'Skunks')
+ax.set_xlabel('Time')
+ax.set_ylabel('Foxes, Skunks')
+
+secax = ax.secondary_yaxis('right', ???)
+secax.set_xlabel('Eagles')
+plt.show()
