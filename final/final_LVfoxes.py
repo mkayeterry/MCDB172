bd5abff63591ab5c0a3108fb53abbde3c69f34a6
Melissa Terry <mkterry@ucsb.edu>
2023-03-03 15:29:30 -0800

    question 1 and 2 complete

--- final/final_LVfoxes.py
+++ final/final_LVfoxes.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import integrate
+from scipy.integrate import odeint
 
 # E = golden eagle (predator)
 E0 = 28 # placeholder
@@ -47,27 +47,23 @@ lambda_f = 7.7e-4
 lambda_p = 7.7e-4
 lambda_s = 2.5e-4
 
-# lambda_i = the rate at which prey i are turned into new predators
-lambda_f = 7.7e-4
-lambda_p = 7.7e-4
-lambda_s = 2.5e-4
-
 # ------------------------------------------------------------------------------------------------
+# Without Pigs
+def LV(state, t):
+    F, S, E = state
 
-# function to calculate derivatives
-def derivative(X, t, phi, sigma, nu, r_f, r_s, r_p, K_f, K_s, K_p, mu_f, mu_s, mu_p, lambda_f, lambda_s, lambda_p, beta_fs, beta_sf):
-    F, S, P, E = X
-    dFdt = ( r_f * F * (1 - ((F + (beta_fs * S)) / K_f)) ) - ( mu_f * ((phi * F) / ((phi * F) + (sigma * S) + P)) * E * F )
-    dSdt = ( r_s * S * (1 - ((S + (beta_sf * F)) / K_s)) ) - ( mu_s * ((sigma * S) / ((phi * F) + (sigma * S) + P)) * E * S )
-    dPdt = ( r_p * P * (1 - (P / K_p)) ) - ( mu_p * (P / ((phi * F) + (sigma * S) + P)) * E * P )
-    dEdt = (((lambda_f * mu_f * phi * (F**2)) + (lambda_s * mu_s * sigma * (S**2)) + (lambda_p * mu_p * (P**2)) * E) / ((phi * F) + (sigma * S) + P)) - (nu * E)
-    return np.array([dFdt, dSdt, dPdt, dEdt])
+    dFdt = r_f * F * (1 - ((F + (beta_fs * S)) / K_f)) - mu_f * ((phi * F) / ((phi * F) + (sigma * S))) * E * F
+    dSdt = r_s * S * (1 - ((S + (beta_sf * F)) / K_s)) - mu_s * ((sigma * S) / ((phi * F) + (sigma * S))) * E * S
+    dEdt = (((lambda_f * mu_f * phi * (F**2)) + (lambda_s * mu_s * sigma * (S**2)) * E) / ((phi * F) + (sigma * S))) - (nu * E)
+    return np.array([dFdt, dSdt, dEdt])
 
+t = np.linspace(0, 100, 15)
 
-t = np.linspace(0, 100, 50)
-X0 = [F0, S0, P0, E0]
-res = integrate.odeint(derivative, X0, t, args = (phi, sigma, nu, r_f, r_s, r_p, K_f, K_s, K_p, mu_f, mu_s, mu_p, lambda_f, lambda_s, lambda_p, beta_fs, beta_sf))
-F, S, P, E = res.T
+state0 = [F0, S0, E0]
+
+state = odeint(LV, state0, t)
+
+F, S, E = state.T
 
 def y1_y2(x):
     return ((2*x)/100)
@@ -75,33 +71,56 @@ def y1_y2(x):
 def y2_y1(x):
     return x*50
 
-fig, ax = plt.subplots(constrained_layout=True)
+fig, ax = plt.subplots()
 ax.plot(t, F, '-', linewidth = 3, label = 'Foxes', color = 'blue')
 ax.plot(t, S, '-', linewidth = 3, label = 'Skunks', color = 'lightgreen')
-ax.plot(t, E, '-', linewidth = 3, label = 'Eagles', color = 'red')
+ax.plot(t, E, '-', linewidth = 3, label = 'Eagles', color = '#ee5727')
 ax.set_xlabel('Time')
 ax.set_ylabel('Foxes, Skunks')
 secax = ax.secondary_yaxis('right', functions = (y1_y2, y2_y1))
 secax.set_ylabel('Eagles')
-# secax.set_ylim(0, 30)
-plt.xlim(0, 100)
-plt.ylim(0, 1400)
-plt.legend()
+secax.set_ylim(0, 30)
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 1400)
+ax.legend()
 plt.show()
+# -------------------------------------------------------------------------------
 
+# With Pigs
+def LV_pigs(state_2, t):
+    F_2, S_2, P_2, E_2 = state_2
 
+    dFdt = (r_f * F_2 * (1 - ((F_2 + (beta_fs * S_2)) / K_f))) - mu_f * ((phi * F_2) / ((phi * F_2) + (sigma * S_2) + (P_2))) * E_2 * F_2
+    dSdt = (r_s * S_2 * (1 - ((S_2 + (beta_sf * F_2)) / K_s))) - mu_s * ((sigma * S_2) / ((phi * F_2) + (sigma * S_2) + (P_2))) * E_2 * S_2
+    dPdt = (r_p * P_2 * (1 - (P_2 / K_p))) - mu_p * (P_2 / ((phi * F_2) + (sigma * S_2) + (P_2))) * E_2 * P_2
+    dEdt = (((lambda_f * mu_f * phi * (F_2**2)) + (lambda_s * mu_s * sigma * (S_2**2)) + (lambda_p * mu_p * (P_2**2)) * E_2) / ((phi * F_2) + (sigma * S_2) + (P_2))) - (nu * E_2)
+    return np.array([dFdt, dSdt, dPdt, dEdt])
 
-fig, ax = plt.subplots(constrained_layout=True)
-ax.plot(t, F, '-', linewidth = 3, label = 'Foxes', color = 'blue')
-ax.plot(t, S, '-', linewidth = 3, label = 'Skunks', color = 'lightgreen')
-ax.plot(t, E, '-', linewidth = 3, label = 'Eagles', color = 'red')
-ax.plot(t, (P/1000), '-', linewidth = 3, label = 'Pigs', color = 'orange')
+state0_2 = [F0, S0, P0, E0]
+
+state_2 = odeint(LV_pigs, state0_2, t)
+
+F_2, S_2, P_2, E_2 = state_2.T
+
+def y1_y2(x):
+    return ((2*x)/100)
+
+def y2_y1(x):
+    return x*50
+
+fig, ax = plt.subplots()
+ax.plot(t, F_2, '-', linewidth = 3, label = 'Foxes', color = 'blue')
+ax.plot(t, S_2, '-', linewidth = 3, label = 'Skunks', color = 'lightgreen')
+ax.plot(t, E_2, '-', linewidth = 3, label = 'Eagles', color = '#ee5727')
+ax.plot(t, P_2/1000, '-', linewidth = 3, label = 'Pigs')
 ax.set_xlabel('Time')
 ax.set_ylabel('Foxes, Skunks')
 secax = ax.secondary_yaxis('right', functions = (y1_y2, y2_y1))
-secax.set_ylabel(r'Pigs ($\frac{1}{1000}$), Eagles')
+secax.set_ylabel('Eagles')
 secax.set_ylim(0, 30)
-plt.xlim(0, 100)
-plt.ylim(0, 1400)
-plt.legend()
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 1400)
+ax.legend()
 plt.show()
+
+
